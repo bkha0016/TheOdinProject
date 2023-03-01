4f6f08d9dc3d17857a981b34e332995ade417223
bkha0016 <95135120+bkha0016@users.noreply.github.com>
2023-03-01 14:00:23 +0700

    adding javascrip2.js for javascript part 2 on the odin project foundation class
    Signed-off-by: bkha0016 <95135120+bkha0016@users.noreply.github.com>

--- Foundations/javasfd/javascript.js
+++ Foundations/javasfd/javascript.js
@@ -37,3 +37,22 @@ console.log(x / y);
 console.log(x * y);
 console.log(x + y);
 console.log(x - y);
+
+console.log(23 + 97);
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10; console.log(a);
+console.log(9 * a);
+
+let b = 7 * a;
+console.log(b);
+
+const MAX = 57;
+const actual = MAX - 13;
+const percentage = actual / MAX;
+
+console.log(percentage);
+
+
+
