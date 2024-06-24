4aafc8e85709a17553ea065ef169cde7ec2f9369
bkha0016 <bryankhang123@gmail.com>
2024-06-24 15:13:39 +0700

    finished with the book section

--- fullStack/js/obj/script.js
+++ fullStack/js/obj/script.js
@@ -99,6 +99,74 @@ function Book(title, author, numPages, isRead) {
 const Hobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read yet");
 Hobbit.info();
 
+// Initialize a constructor function for a new Hero
+function Hero(name, level) {
+    this.name = name;
+    this.level = level;
+
+}
+
+// Now we can create a new instance with new
+let hero1 = new Hero("Bjorn", 1);
+console.log(hero1.__proto__);
+console.log(hero1.__proto__.__proto__);
+
+console.log(Object.getPrototypeOf(hero1));
+
+// Add greet method to the Hero prototype
+Hero.prototype.greet = function () {
+    return `${this.name} says hello.`;
+}
+
+console.log(hero1.greet());
+
+// We can use the call() method to copy over properties from one constructor into another constructor
+// Intialize Warrior constructor
+function Warrior(name, level, weapon) {
+    // Chain constructor with call
+    Hero.call(this, name, level);
+
+    // Add a new property
+    this.weapon = weapon;
+}
+
+// Initialize Healer constructor
+function Healer(name, level, spell) {
+    Hero.call(this, name, level);
+
+    this.spell = spell;
+}
+
+
+// you can also use prototype to add the attack() method to warrior
+// and the heal() method to healer
+Warrior.prototype.attack = function () {
+    return `${this.name} attacks with the ${this.weapon}`;
+}
+
+Healer.prototype.heal = function() {
+    return `${this.name} casts ${this.spell}`;
+}
+
+// we'll create our chara with the two new chara classes available
+hero1 = new Warrior("Bjorn", 1, "axe");
+let hero2 = new Healer("Kanin", 1, "cure");
+
+
+console.log(hero1.attack());
+console.log(hero2.heal());
+
+// however the call methods only gets the constructor variable not the methods from their parents
+// required the usage of Object.setPropertyOf() to link the properties in Hero constructor to the warrior and healer constructurs, making sure to put it before any additional methods
+// Link prototypes and add prototype methods
+Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+Object.setPrototypeOf(Healer.prototype, Hero.prototype);
+
+console.log(hero1.greet());
+console.log(hero2.greet());
+
+
+
 
 
 
